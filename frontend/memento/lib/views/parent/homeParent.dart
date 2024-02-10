0526e421afb297896ba597378bce84c49004347e
agrim saxena <agrim.12saxena@gmail.com>
2024-02-10 10:27:42 +0530

    scan-implemented

--- frontend/memento/lib/views/parent/homeParent.dart
+++ frontend/memento/lib/views/parent/homeParent.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:memento/views/patient/eventEditing.dart';
 import 'package:memento/widgets/eventCard.dart';
 import 'package:provider/provider.dart';
@@ -16,10 +18,35 @@ class HomeParent extends StatefulWidget {
 }
 
 class _HomeParentState extends State<HomeParent> {
+  String qrResult = "Not Yet Scanned";
+
+   Future<void> scanQRCode() async {
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Cancel", false, ScanMode.QR);
+      if (!mounted) return;
+      setState(() {
+        qrResult = qrCode;
+      });
+    } on PlatformException {
+      qrResult = "Failed to get platform version";
+    }
+  }
   @override
   Widget build(BuildContext context) {
     final events = Provider.of<EventProvider>(context).events;
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Memento"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              scanQRCode();
+            },
+            icon: Icon(Icons.qr_code),
+          ),
+        ],
+      ),
       body: Container(
         child: SfCalendar(
           view: CalendarView.month,
